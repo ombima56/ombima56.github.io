3455e757937c2cdf500a5d8ce428caedbbc6395b
ombima56 <ombimahillary6@gmail.com>
2024-11-14 15:37:51 +0300

    chore: remove redundant lines

--- js/home_page.js
+++ js/home_page.js
@@ -98,19 +98,3 @@ document.addEventListener('DOMContentLoaded', () => {
     navLinks.classList.toggle('active');
   });
 });
-
-/**
-  * Init typed.js
-  */
-const selectTyped = document.querySelector('.typed');
-if (selectTyped) {
-  let typed_strings = selectTyped.getAttribute('data-typed-items');
-  typed_strings = typed_strings.split(',');
-  new Typed('.typed', {
-    strings: typed_strings,
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000
-  });
-}
